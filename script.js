00f9abe03f7f6d041dd4e497825d0c5dfa711c90
techflight <144474241+techflight@users.noreply.github.com>
2023-09-30 22:59:02 +0530

    Welcome to Techflight

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const learnMoreBtn = document.getElementById("learnMoreBtn");
+
+    learnMoreBtn.addEventListener("click", function() {
+        alert("Thank you for wanting to learn more about me!");
+    });
+});
